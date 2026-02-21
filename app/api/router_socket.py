0cb751815b95a0b87e0ee8a4055a3a90a8c03191
shalkovk <kshalkovskiy@gmail.com>
2026-02-21 23:43:45 +0500

    added router socket connection

---
+++ app/api/router_socket.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from typing import Dict
+
+router = APIRouter(prefix="/ws/chat")
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: Dict[int, Dict[int, WebSocket]] = {}
+
+    async def connect(self, websocket: WebSocket, room_id: int, user_id: int):
+        await websocket.accept()
+        if room_id not in self.active_connections:
+            self.active_connections[room_id] = {}
+        self.active_connections[room_id][user_id] = websocket
+
+    def disconnect(self, room_id: int, user_id: int):
+        if room_id in self.active_connections and user_id in self.active_connections[room_id]:
+            del self.active_connections[room_id][user_id]
+            if not self.active_connections[room_id]:
+                del self.active_connections[room_id]
+
+    async def broadcast(self, message: str, room_id: int, sender_id: int):
+        if room_id in self.active_connections:
+            for user_id, connection in self.active_connections[room_id].items():
+                message_with_class = {
+                    "text": message,
+                    "is_self": user_id == sender_id
+                }
+                await connection.send_json(message_with_class)
