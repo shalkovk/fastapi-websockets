5febf032a5c5c68e92921606cf5172a3c590df6c
shalkovk <kshalkovskiy@gmail.com>
2026-02-22 23:49:24 +0500

    added endpoint for router socket

--- app/api/router_socket.py
+++ app/api/router_socket.py
@@ -28,3 +28,17 @@ class ConnectionManager:
                     "is_self": user_id == sender_id
                 }
                 await connection.send_json(message_with_class)
+
+
+@router.websocket("/{room_id}/{user_id}")
+async def websocket_endpoint(websocket: WebSocket, room_id: int, user_id: int, username: str):
+    await manager.connect(websocket, room_id, user_id)
+    await manager.broadcast(f"{username} (ID: {user_id}) присоединился к чату.", room_id, user_id)
+
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.broadcast(f"{username} (ID: {user_id}): {data}", room_id, user_id)
+    except WebSocketDisconnect:
+        manager.disconnect(room_id, user_id)
+        await manager.broadcast(f"{username} (ID: {user_id}) покинул чат.", room_id, user_id)
